c3ece28a5a1834d67ff851df1c87ed6bfb3c5187
Sam Culley <sam@samculley.co.uk>
2022-03-21 11:43:18 +0000

    fixing locals

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  cloud_user_pw = var.cloud_user_pw ? var.cloud_user_pw : random_password.this_cloud_user[0].result
-  root_pw = var.root_pw ? var.root_pw : random_password.this_root_user[0].result
+  cloud_user_pw = var.cloud_user_pw != "" ? var.cloud_user_pw : random_password.this_cloud_user[0].result
+  root_pw = var.root_pw != "" ? var.root_pw : random_password.this_root_user[0].result
 }
