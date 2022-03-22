0823b6d355e747ec5a2b6b9c2f8793bae266bfb1
Sam Culley <sam@samculley.co.uk>
2022-03-22 08:30:40 +0000

    further chanes to get working

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
 # generate the cloud_init file into the ubuntu image
 data "template_file" "user_data" {
   template = templatefile("${path.module}/templates/cloudinit/cloud_init.cfg.tpl", {
-    cloud_user_pw = local.cloud_user_pw
+    cloud_user_pw = sha512(var.cloud_user_pw)
     cloud_user_public_key = var.cloud_user_public_key
-    root_pw = local.root_pw
+    root_pw = sha512(var.root_pw)
   })
 }
 
