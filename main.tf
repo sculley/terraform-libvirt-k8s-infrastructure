fcca9eed1ce78bd18da88b4ce71d21445d1ae1bf
Sam Culley <sam@samculley.co.uk>
2022-03-31 11:31:01 +0100

    fixing the previous releases

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ module "controlplane_nodes" {
   source = "./modules/controlplanes"
 
   # node config
-  node_count = try(var.controlplane_node_group.count, var.controlplane_node_group_defaults.count)
-  memory = try(var.controlplane_node_group.memory, var.controlplane_node_group_defaults.memory)
-  vcpu = try(var.controlplane_node_group.vcpu, var.controlplane_node_group_defaults.vcpu)
-  os_source = try(var.controlplane_node_group.os_source, var.controlplane_node_group_defaults.os_source)
+  node_count = try(var.controlplane_settings.count, var.controlplane_node_group_defaults.count)
+  memory = try(var.controlplane_settings.memory, var.controlplane_node_group_defaults.memory)
+  vcpu = try(var.controlplane_settings.vcpu, var.controlplane_node_group_defaults.vcpu)
+  os_source = try(var.controlplane_settings.os_source, var.controlplane_node_group_defaults.os_source)
 
   # disk/volume config
   cloud_init_network_config = var.cloud_init_network_config != "" ? var.cloud_init_network_config : data.template_file.network_config.rendered
@@ -17,10 +17,10 @@ module "nodes" {
   source = "./modules/nodes"
 
   # node config
-  node_count = try(var.node_group.count, var.node_group_defaults.count)
-  memory = try(var.node_group.memory, var.node_group_defaults.memory)
-  vcpu = try(var.node_group.vcpu, var.node_group_defaults.vcpu)
-  os_source = try(var.node_group.os_source, var.node_group_defaults.os_source)
+  node_count = try(var.node_group_settings.count, var.node_group_defaults.count)
+  memory = try(var.node_group_settings.memory, var.node_group_defaults.memory)
+  vcpu = try(var.node_group_settings.vcpu, var.node_group_defaults.vcpu)
+  os_source = try(var.node_group_settings.os_source, var.node_group_defaults.os_source)
 
   # disk/volume config
   cloud_init_network_config = var.cloud_init_network_config != "" ? var.cloud_init_network_config : data.template_file.network_config.rendered
